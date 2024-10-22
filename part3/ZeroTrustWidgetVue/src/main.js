97082133f0a7b3982a0806f6c092bdfddfe1dd0e
Andrei <150835122+AndreiRozhavskii@users.noreply.github.com>
2024-10-22 07:59:07 +0300

    Update main.js

--- part3/ZeroTrustWidgetVue/src/main.js
+++ part3/ZeroTrustWidgetVue/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createVuetify } from 'vuetify';
-import 'vuetify/styles'; // Импорт стилей Vuetify
-import '@mdi/font/css/materialdesignicons.css'; // Импорт иконок
+import 'vuetify/styles'; 
+import '@mdi/font/css/materialdesignicons.css'; 
 
 const app = createApp(App);
 const vuetify = createVuetify();
